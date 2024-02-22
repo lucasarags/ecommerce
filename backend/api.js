579113871bce62362987181b5b92e41d94b7dd74
lucas <lucas.arags@gmail.com>
2024-02-22 14:53:14 -0300

    ajeitei .env .gitignore e diretorios

--- backend/api.js
+++ backend/api.js
@@ -1,7 +1,7 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
-import pool from "../db/db.mjs";
+import pool from "./db/db.mjs";
 
 const app = express();
 
